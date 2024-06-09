aa448a2ad37885b0771661cfa302683861a7a5b3
Merel1n <ineskamit@gmail.com>
2024-06-09 12:12:09 +0000

    Deploying to gh-pages from @ Merel1n/blandEnd-16@89e647b781ae1736e1993ab0f5c31885731976e3 🚀

---
+++ index.3d6f484f.js
@@ -0,0 +1,2 @@
+[document.querySelector("#task-form"),document.querySelector("#task-list")][0].addEventListener("submit",(function(e){e.preventDefault();var t={};new FormData(e.currentTarget).forEach((function(e,r){t[r]=e}))}));
+//# sourceMappingURL=index.3d6f484f.js.map
